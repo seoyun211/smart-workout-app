32879dbe12bc3dbc444bb1837a37cfdfa77b0c44
lyt928 <dlaxor1029@gmail.com>
2025-03-16 00:11:13 +0900

    Update nanum.py

--- nanum.py
+++ nanum.py
@@ -3,4 +3,14 @@ from kivy.uix.label import Label
 from kivy.core.text import LabelBase
 
 # 한글 폰트를 등록합니다
-LabelBase.register(name="NanumGothic", fn_regular="NanumGothic.ttf")
+LabelBase.register(name="NanumGothic", fn_regular="font/NanumGothic.ttf")
+
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.core.text import LabelBase
+
+# 폰트 등록
+LabelBase.register(name="NanumGothic", fn_regular="font/NanumGothic.ttf")
+
